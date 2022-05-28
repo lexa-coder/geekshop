45ddeff858a98a4210874d04391b4d8c87e1f24a
geekbrains <info@geekbrains.ru>
2022-05-28 15:06:06 +0400

    html: add static settings --->  STATICFILES_DIRS urls ---> urlpatterns views ---> index, products, contact

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.index),
+    path('products/', views.products),
+    path('contact/', views.contact),
 ]

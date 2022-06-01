e5d95269d8a54c9befd23285d4562559760fab81
geekbrains <info@geekbrains.ru>
2022-06-01 07:53:30 +0400

    test django filters

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    cotext = {
+        'user_list': [
+            {
+                'first_name': 'alexey',
+                'last_name': 'papshev',
+                'age': 30
+
+            },
+            {
+                'first_name': 'oleg',
+                'last_name': 'maslov',
+                'age': 31
+
+            }
+        ]
+    }
+    return render(request, 'mainapp/index.html', cotext)
 
 
 def products(request):
